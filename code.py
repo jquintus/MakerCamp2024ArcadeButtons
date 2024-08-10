b8b6ebfd7b8f903f4b30134eb43f02ba8db3db6c
Josh Quintus <josh.quintus@gmail.com>
2024-08-10 18:06:39 -0400

    Get rotary encoder sample to work

--- code.py
+++ code.py
@@ -1,21 +1,45 @@
 """Main entry point for all CircuitPython Code. This script will enable a
 Feather RP2040 to act like a keyboard with 4 buttons and a volume knob."""
 
+
+import board
+from adafruit_seesaw import seesaw, rotaryio, digitalio
+
 print("Hello World!")
-print ("Josh is here")
-print ("Josh is here")
-print("Hello World!")
-print ("Josh is here")
-print ("Josh is here")
-print("Hello World!")
-print ("Josh is here")
-print ("Josh is here")
-print("Hello World!")
-print ("Josh is here")
-print ("Josh is here")
-print("Hello World!")
-print ("Josh is here")
-print ("Josh is here")
-print("Hello World!")
-print ("Josh is here")
-print ("Josh is here")
+
+
+i2c = board.I2C()  # uses board.SCL and board.SDA
+# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+seesaw = seesaw.Seesaw(i2c, addr=0x36)
+
+seesaw_product = (seesaw.get_version() >> 16) & 0xFFFF
+print("Found product {}".format(seesaw_product))
+if seesaw_product != 4991:
+    print("Wrong firmware loaded?  Expected 4991")
+
+# Configure seesaw pin used to read knob button presses
+# The internal pull up is enabled to prevent floating input
+seesaw.pin_mode(24, seesaw.INPUT_PULLUP)
+button = digitalio.DigitalIO(seesaw, 24)
+
+button_held = False
+
+encoder = rotaryio.IncrementalEncoder(seesaw)
+last_position = None
+
+while True:
+    # negate the position to make clockwise rotation positive
+    position = -encoder.position
+
+    if position != last_position:
+        last_position = position
+        print("Position: {}".format(position))
+
+    if not button.value and not button_held:
+        button_held = True
+        print("Button pressed")
+
+    if button.value and button_held:
+        button_held = False
+        print("Button released")
+
