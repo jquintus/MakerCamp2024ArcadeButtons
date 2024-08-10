f525a0d06304e6212a78cf3a7b6b01f2d46779ea
Josh Quintus <josh.quintus@gmail.com>
2024-08-10 17:43:49 -0400

    Fix bad newlines

--- code.py
+++ code.py
@@ -1,9 +1,20 @@
-"""Main entry point for all CircuitPython Code. This script will enable a
-Feather RP2040 to act like a keyboard with 4 buttons and a volume knob."""
+"""Main entry point for all CircuitPython Code. This script will enable a Feather RP2040 to act like a keyboard with 4 buttons and a volume knob."""
 
-print("Hello World!") print ("Josh is here") print ("Josh is here")
-print("Hello World!") print ("Josh is here") print ("Josh is here")
-print("Hello World!") print ("Josh is here") print ("Josh is here")
-print("Hello World!") print ("Josh is here") print ("Josh is here")
-print("Hello World!") print ("Josh is here") print ("Josh is here")
-print("Hello World!") print ("Josh is here") print ("Josh is here")
+print("Hello World!")
+print ("Josh is here")
+print ("Josh is here")
+print("Hello World!")
+print ("Josh is here")
+print ("Josh is here")
+print("Hello World!")
+print ("Josh is here")
+print ("Josh is here")
+print("Hello World!")
+print ("Josh is here")
+print ("Josh is here")
+print("Hello World!")
+print ("Josh is here")
+print ("Josh is here")
+print("Hello World!")
+print ("Josh is here")
+print ("Josh is here")
