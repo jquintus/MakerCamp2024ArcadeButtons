7899217df85cf08ce3d8da563f64f73e6f2684c4
Josh Quintus <josh.quintus@gmail.com>
2024-08-10 17:50:10 -0400

    Break up long line

--- code.py
+++ code.py
@@ -1,4 +1,5 @@
-"""Main entry point for all CircuitPython Code. This script will enable a Feather RP2040 to act like a keyboard with 4 buttons and a volume knob."""
+"""Main entry point for all CircuitPython Code. This script will enable a
+Feather RP2040 to act like a keyboard with 4 buttons and a volume knob."""
 
 print("Hello World!")
 print ("Josh is here")
