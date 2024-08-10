ecd7c70be2f7aa0b521e145cbed5a56a80112172
Josh Quintus <josh.quintus@gmail.com>
2024-08-10 18:35:40 -0400

    Refactored creation of Encoder into a function

--- code.py
+++ code.py
@@ -5,26 +5,26 @@ Feather RP2040 to act like a keyboard with 4 buttons and a volume knob."""
 import board
 from adafruit_seesaw import seesaw, rotaryio, digitalio
 
-print("Hello World!")
+def init_rotary_encoder():
+    i2c = board.I2C()  # uses board.SCL and board.SDA
+    # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+    seesawBoard = seesaw.Seesaw(i2c, addr=0x36)
 
+    seesaw_product = (seesawBoard.get_version() >> 16) & 0xFFFF
+    print(f"Found product {seesaw_product}")
+    if seesaw_product != 4991:
+        print("Wrong firmware loaded?  Expected 4991")
 
-i2c = board.I2C()  # uses board.SCL and board.SDA
-# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
-seesaw = seesaw.Seesaw(i2c, addr=0x36)
+    # Configure seesaw pin used to read knob button presses
+    # The internal pull up is enabled to prevent floating input
+    seesawBoard.pin_mode(24, seesawBoard.INPUT_PULLUP)
+    button = digitalio.DigitalIO(seesawBoard, 24)
+    encoder = rotaryio.IncrementalEncoder(seesawBoard)
 
-seesaw_product = (seesaw.get_version() >> 16) & 0xFFFF
-print(f"Found product {seesaw_product}")
-if seesaw_product != 4991:
-    print("Wrong firmware loaded?  Expected 4991")
+    return encoder, button
 
-# Configure seesaw pin used to read knob button presses
-# The internal pull up is enabled to prevent floating input
-seesaw.pin_mode(24, seesaw.INPUT_PULLUP)
-button = digitalio.DigitalIO(seesaw, 24)
-
+(encoder, button) = init_rotary_encoder()
 BUTTON_HELD = False
-
-encoder = rotaryio.IncrementalEncoder(seesaw)
 LAST_POSITION = encoder.position
 
 while True:
@@ -38,10 +38,6 @@ while True:
     elif (position_delta > 0):
         LAST_POSITION = position
         print("down")
-
-    if position != LAST_POSITION:
-        LAST_POSITION = position
-        print(f"Position: {position}")
 
     if not button.value and not BUTTON_HELD:
         BUTTON_HELD = True
