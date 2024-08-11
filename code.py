50b2e7cac00e91616a47a74f084b678f2f3bf437
Josh Quintus <josh.quintus@gmail.com>
2024-08-11 17:40:34 -0400

    Add support for buttons

--- code.py
+++ code.py
@@ -1,15 +1,24 @@
 """Main entry point for all CircuitPython Code. This script will enable a
 Feather RP2040 to act like a keyboard with 4 buttons and a volume knob."""
 
+import time
 import board
-from adafruit_seesaw import seesaw, rotaryio, digitalio
-
 import usb_hid
+import digitalio as board_digitalio
+
+from adafruit_seesaw import seesaw, rotaryio, digitalio
+from adafruit_seesaw.pwmout import PWMOut
 from adafruit_hid.consumer_control_code import ConsumerControlCode
 from adafruit_hid.consumer_control import ConsumerControl
 
-def init_rotary_encoder():
+
+def init_i2c():
+    print("Initializing I2C...")
     i2c = board.I2C()  # uses board.SCL and board.SDA
+    return i2c
+
+def init_rotary_encoder(i2c):
+    print("Initializing rotary encoder...")
     seesawBoard = seesaw.Seesaw(i2c, addr=0x36)
 
     seesaw_product = (seesawBoard.get_version() >> 16) & 0xFFFF
@@ -25,15 +34,51 @@ def init_rotary_encoder():
 
     return encoder, button
 
+def init_button(arcade_qt, pin):
+    print("Initializing button...")
+    button = digitalio.DigitalIO(arcade_qt, pin)
+    button.direction = board_digitalio.Direction.INPUT
+    button.pull = board_digitalio.Pull.UP
+
+    return button
+
+def init_buttons(i2c):
+    print("Initializing leds and buttons...")
+    arcade_qt = seesaw.Seesaw(i2c, addr=0x3A)
+
+    print("Initializing leds...")
+    led1 = digitalio.DigitalIO(arcade_qt, 12)
+    led2 = digitalio.DigitalIO(arcade_qt, 13)
+    led3 = digitalio.DigitalIO(arcade_qt, 0)
+    led4 = digitalio.DigitalIO(arcade_qt, 1)
+
+    print("Initializing buttons...")
+    button1 = init_button(arcade_qt, 18)
+    button2 = init_button(arcade_qt, 19)
+    button3 = init_button(arcade_qt, 20)
+    button4 = init_button(arcade_qt, 2)
+
+    return ((led1, led2, led3, led4), (button1, button2, button3, button4))
+
 def init_hid():
+    print("Initializing HID...")
     cc = ConsumerControl(usb_hid.devices)
     return cc
 
-(encoder, button) = init_rotary_encoder()
+i2c = init_i2c()
 cc = init_hid()
+(encoder, button) = init_rotary_encoder(i2c)
+((led1, led2, led3, led4), (button1, button2, button3, button4)) = init_buttons(i2c)
+
 BUTTON_HELD = False
 LAST_POSITION = encoder.position
 
+button1_held = False
+button2_held = False
+button3_held = False
+button4_held = False
+
+print("Ready to start")
 while True:
     position = encoder.position
     position_delta = LAST_POSITION - position
@@ -67,3 +112,39 @@ while True:
 
     if button.value and BUTTON_HELD:
         BUTTON_HELD = False
+
+    # Button 1
+    if not button1.value and not button1_held:
+        button1_held = True
+        led1.value = True
+
+    if button1.value and button1_held:
+        button1_held = False
+        led1.value = False
+
+    # Button 2
+    if not button2.value and not button2_held:
+        button2_held = True
+        led2.value = True
+
+    if button2.value and button2_held:
+        button2_held = False
+        led2.value = False
+
+    # Button 3
+    if not button3.value and not button3_held:
+        button3_held = True
+        led3.value = True
+
+    if button3.value and button3_held:
+        button3_held = False
+        led3.value = False
+
+    # Button 4
+    if not button4.value and not button4_held:
+        button4_held = True
+        led4.value = True
+
+    if button4.value and button4_held:
+        button4_held = False
+        led4.value = False
--- readme.md
+++ readme.md
@@ -6,6 +6,7 @@ a volume knob based off of Adafruit's feather boards.
 * [CircuitPython v9.11](https://circuitpython.org/board/adafruit_feather_rp2040/)
 * [Adafruit Feather RP2040](https://www.adafruit.com/product/4884)
 * [Adafruit I2C Stemma QT Rotary Encoder Breakout with Encoder](https://www.adafruit.com/product/5880)
+* [Adafruit LED Arcade Button 1x4 - STEMMA QT I2C Breakout - STEMMA QT / Qwiic](https://www.adafruit.com/product/5296)
 * [USB C Round Panel Mount Extension Cable](https://www.adafruit.com/product/4218)
 * Arcade Buttons
     * [Green](https://www.adafruit.com/product/3487)
@@ -61,6 +62,7 @@ repository, which includes all dependencies. The board will restart.
 
 * [Welcome to CircuitPython](https://learn.adafruit.com/welcome-to-circuitpython/overview)
 * [Adafruit I2C QT Rotary Encoder](https://learn.adafruit.com/adafruit-i2c-qt-rotary-encoder/python-circuitpython)
+* [Adafruit LED Arcade Button 1x4 STEMMA QT](https://learn.adafruit.com/adafruit-led-arcade-button-qt)
 * [Introducing Adafruit Feather RP2040](https://learn.adafruit.com/adafruit-feather-rp2040-pico/overview)
 * [CircuitPython HID Keyboard and Mouse](https://learn.adafruit.com/adafruit-feather-m0-express-designed-for-circuit-python-circuitpython/circuitpython-hid-keyboard-and-mouse)
     * This is for differnt hardware, but the demo code should work pretty well.
